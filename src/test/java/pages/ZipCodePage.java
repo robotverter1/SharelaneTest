e7d44673693c6b8b5edcc4b9cf55a5541f3c9fd3
Svyatoslav <122548106+robotverter1@users.noreply.github.com>
2026-09-11 13:43:15 +0300

    first commit

---
+++ src/test/java/pages/ZipCodePage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ZipCodePage {
+
+    private WebDriver driver;
+    private Wait<WebDriver> wait;
+
+    private final By zipCodeField = By.name("zip_code");
+
+    private final By continueButton = By.cssSelector("[value='Continue']");
+
+    private final By errorMessage = By.cssSelector(".error_message");
+
+    private final By registrationFormTitle = By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[7]/td/span");
+
+    private ZipCodePage (WebDriver driver){
+        this.driver=driver;
+        this.wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    public ZipCodePage open(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        return this;
+    }
+
+    public ZipCodePage enterZipCode(String zip){
+        driver.findElement(zipCodeField).clear();
+        driver.findElement(zipCodeField).sendKeys(zip);
+        return this;
+    }
+    public ZipCodePage clickContinue(){
+        driver.findElement(continueButton).click();
+        return this;
+    }
+    public String getError(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
+        return
+    }
+}
